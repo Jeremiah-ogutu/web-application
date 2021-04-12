2dcb0bf1960b34241ba57035d1acacead1f9dad4
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-04-12 13:37:11 +0300

    button fuctions

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var daysOfTheWeek =['Sunday','Monday','tuesday','Wendsday','Thursday','Friday','Satarday'];
 var akanMale=['Kwasi', 'Kwadwo','Kwabena','Kwaku', 'yaw','Kofi','Kwame'];
 var akanFemale=['Akosua','Adwoa','Abena','Akua','Yaa','Afua', 'Ama'];
+function results() {
+  var month =parseInt(document.getElementById("month").value);
+  var year =parseInt(document.getElementById("year").value);
+  var day =parseInt(document.getElementById ("day").value);
+  var date =new Date(year +"/"+month+"/"+ day);
 
 
 
