7d25bb32f4f732c9cf7ebfe1d2f287fb62785ec5
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-04-12 13:35:08 +0300

    array

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+var daysOfTheWeek =['Sunday','Monday','tuesday','Wendsday','Thursday','Friday','Satarday'];
+var akanMale=['Kwasi', 'Kwadwo','Kwabena','Kwaku', 'yaw','Kofi','Kwame'];
+var akanFemale=['Akosua','Adwoa','Abena','Akua','Yaa','Afua', 'Ama'];
 
 
 
